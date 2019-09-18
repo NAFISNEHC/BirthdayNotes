b6ebab1300e9d88f123554d5e175919a40ff1a19
allahbin <569693942@qq.com>
2019-09-19 00:12:38 +0800

    对代码的文件进行了修改，加上了toast的包

---
+++ lib/page/MainPage.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import './HomePage.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        backgroundColor: Colors.transparent, //把scaffold的背景色改成透明
+        appBar: null,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              margin: EdgeInsets.only(top: 60.0),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                    image: new AssetImage('images/logo2.png')),
+              ),
+              height: 180.0,
+            ),
+            new Container(
+              margin: EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: new Text(
+                'Birthday Notes',
+                style: TextStyle(color: Colors.black87, fontSize: 30.0),
+                textAlign: TextAlign.center,
+              ),
+              width: 300.0,
+            ),
+            new Container(
+              margin: EdgeInsets.only(
+                top: 50.0,
+              ),
+              child: new Text(
+                'Each birthday is a milestone we touch along life\'s way.\nAffectionate birthday greetings!',
+                style: TextStyle(color: Colors.black87, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              ),
+              width: 300.0,
+            ),
+            new Container(
+              margin: EdgeInsets.only(
+                top: 160.0,
+              ),
+              child: new Material(
+                color: Colors.black87,
+                child: new MaterialButton(
+                  child: new Text(
+                    'Take Notes',
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                  minWidth: 200.0,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                          builder: (context) => new HomePage()),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

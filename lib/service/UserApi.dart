1380fa5bd85a7cbbbd24b8ffc597f50b288d1738
allahbin <569693942@qq.com>
2019-10-11 00:24:56 +0800

    完成HTTP请求

--- lib/service/UserApi.dart
+++ lib/service/UserApi.dart
@@ -1,8 +1,12 @@
-import 'package:flutter_app/utils/HttpRequest.dart';
+import 'package:flutter_app/utils/HttpUtils.dart';
 
 class UserApi {
   // 获取用户列表数据，这里要带上async哦
   static getUserList(params) async {
-    return HttpRequest.post("http://47.110.46.62:3000/mock/40/regSys/api/user/list", );
+    return HttpUtils.request(
+        "http://47.110.46.62:3000/mock/40/regSys/api/user/list",
+        method: HttpUtils.POST,
+        data: params
+    );
   }
 }
